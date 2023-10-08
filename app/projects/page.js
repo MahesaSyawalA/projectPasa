911e93efbd97fd90d7f74976f1a18d61470c03db
Iqbal Pasha <iqbalpasha6a@gmail.com>
2023-10-08 12:10:15 +0700

    fix bug

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ProjectCard from "./../components/ProjectCard";
 
-export const projectsData = [
+const projectsData = [
   {
     id: 1,
     title: "Rent Workspace Website",
